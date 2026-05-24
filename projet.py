4c3d30f2beb0239ef7fa3321ad94f2e7db7d0d29
yassine <yassine.ouahbi04@gmail.com>
2026-05-24 12:20:31 +0200

    class_atom

--- projet.py
+++ projet.py
@@ -27,3 +27,24 @@ REQUIRED_BONDS = {
 # On s'accorde une marge de tolérance de 0.6 Å pour considérer qu'une liaison hydrogène est valide.
 DIST_MIN = 2.4
 DIST_MAX = 3.6
+
+# ==========================================
+# MODELISATION DES CLASSES (Atom, Nucleotide, RNAMolecule)
+# ==========================================
+
+class Atom:
+    """ On définit ce qu'est un atome, extrait du fichier PDB, avec un nom (Carbone, Azote, etc.) et des coordonnées. """
+    def __init__(self, name, x, y, z):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def distance_to(self, other_atom):
+        """ On calcule la distance euclidienne 3D entre l'atome actuel (self) et un autre atome (other_atom). On applique la formule qui est la suivante : d (ATOM 1 /ATOM 2)= sqrt((x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2). L'ordre des atomes n'a pas d'importance pour le calcul de la distance. """ 
+        return math.sqrt(
+            (self.x - other_atom.x) ** 2 +
+            (self.y - other_atom.y) ** 2 +
+            (self.z - other_atom.z) ** 2
+        )
+    
